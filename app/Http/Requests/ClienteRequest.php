a517d5b01ce14d4e4b5ff94e78ef3e799cb01dbc
RinghettiFranco <ringhetti.franco@gmail.com>
2022-09-22 15:19:59 -0300

    Revert "Rompiendo 2"
    This reverts commit e28995371a17cee0dc6a089e67396ce97ee867b9.

---
+++ app/Http/Requests/ClienteRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\RequiredIf;
+
+class ClienteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nombre' => 'required',
+            'apellido'=> 'required',
+            'doctipo' => 'required',
+            'docnro' => 'required|numeric',
+            'email' => 'required|email',
+            'IVA' => 'required',
+            'CUIT' => new RequiredIf($this->IVA != 'Consumidor final'),'numeric'
+        ];
+    }
+}
